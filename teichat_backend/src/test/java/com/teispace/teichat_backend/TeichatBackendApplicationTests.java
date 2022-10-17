22ffcb61aff936581de4c8f8c4de07d56a714fbf
Krishna Adhikari <krishna.forkhaki@gmail.com>
2022-10-17 21:18:28 +0545

    initial commit

---
+++ teichat_backend/src/test/java/com/teispace/teichat_backend/TeichatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teispace.teichat_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeichatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
